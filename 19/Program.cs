b1e3ab398562cdaa00ed1599ae7ca1ba9b6f276a
Максим <Mt.Pm@yandex.ru>
2022-11-12 13:19:05 +0700

    Задания готовы на отправку

---
+++ 19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine()!);
+System.Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine()!);
+int res=Natural(num1,num2);
+System.Console.WriteLine("Результат возведения в степень второго числа:" + res);
+System.Console.Write("Для продолжения нажмите любую клавишу: ");
+Console.ReadKey();
+//Функция:
+int Natural (int a, int b)
+{
+int res = Convert.ToInt32(Math.Pow(a, b));
+return res;
+}
---
+++ 21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+// Через строку решать нельзя.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+System.Console.WriteLine("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine()!);
+int result= Summ(num);
+System.Console.WriteLine("Сумма всех чисел введенного числа: " + result);
+System.Console.Write("Для продолжения нажмите: ");
+Console.ReadKey();
+
+//Функция:
+int Summ (int a)
+{
+int sum=0;
+int count = a.ToString().Length;
+for (int i= 0; i < count; i++)
+{
+    int curr = a % 10;
+    a /= 10;
+    sum += curr;
+}
+return sum;
+}
+
+System.Console.Write("Для продолжения нажмите любую клавишу: ");
+Console.ReadKey();
